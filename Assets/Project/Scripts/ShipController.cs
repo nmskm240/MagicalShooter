1c7eae9247f5f19dab0f580654531f41a9648b06
nmskm240 <bokaronogod@gmail.com>
2021-12-27 20:31:15 +0900

    fix ship move

--- Assets/Project/Scripts/ShipController.cs
+++ Assets/Project/Scripts/ShipController.cs
@@ -4,33 +4,38 @@ public class ShipController : MonoBehaviour
 {
     [SerializeField, Range(1, 5)]
     private float _speedCompensation = 0.1f;
-    [SerializeField, Range(0.25, 1)]
+    [SerializeField, Range(0.25f, 1)]
     private float _fireInterval = 1;
     [SerializeField]
     private GameObject _bullet;
     private float _time;
+    private Rigidbody2D _rigidbody;
+
+    private void Awake()
+    {
+        _rigidbody = GetComponent<Rigidbody2D>();
+    }
 
     private void Update()
     {
         _time += Time.deltaTime;
-        var x = Input.GetAxis("Horizontal");
-        var y = Input.GetAxis("Vertical");
-        Move(x, y);
-        if(_fireInterval < _time && Input.GetKey(KeyCode.Space)) 
+        if (_fireInterval < _time && Input.GetKey(KeyCode.Space))
         {
             _time = 0;
             Fire();
         }
     }
 
-    private void Move(float x, float y)
+    private void FixedUpdate()
     {
-        var speed = new Vector3(x, y, 0);
-        transform.position += speed.normalized * Time.deltaTime * _speedCompensation;
+        var x = Input.GetAxis("Horizontal");
+        var y = Input.GetAxis("Vertical");
+        var dir = new Vector2(x, y).normalized;
+        _rigidbody.velocity = dir * _speedCompensation;
     }
 
     private void Fire()
     {
-        Instantiate(_bullet, transform.position, Quaternion.identity);
+        Instantiate(_bullet, transform.position, transform.rotation);
     }
 }
