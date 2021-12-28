4d1d4b94c31c3fe342c52655563c69af5c6203bf
nmskm240 <bokaronogod@gmail.com>
2021-12-28 10:19:55 +0900

    create ship data

--- Assets/Project/Scripts/ShipController.cs
+++ Assets/Project/Scripts/ShipController.cs
@@ -2,24 +2,16 @@ using UnityEngine;
 
 public class ShipController : MonoBehaviour
 {
-    [SerializeField, Range(1, 5)]
-    private float _speedCompensation = 0.1f;
-    [SerializeField, Range(0.25f, 1)]
-    private float _fireInterval = 1;
+    [SerializeField]
+    private ShipData _data;
     [SerializeField]
     private GameObject _bullet;
     private float _time;
-    private Rigidbody2D _rigidbody;
-
-    private void Awake()
-    {
-        _rigidbody = GetComponent<Rigidbody2D>();
-    }
 
     private void Update()
     {
         _time += Time.deltaTime;
-        if (_fireInterval < _time && Input.GetKey(KeyCode.Space))
+        if (_data.FireRate < _time && Input.GetKey(KeyCode.Space))
         {
             _time = 0;
             Fire();
@@ -28,10 +20,10 @@ public class ShipController : MonoBehaviour
 
     private void FixedUpdate()
     {
-        var x = Input.GetAxis("Horizontal");
-        var y = Input.GetAxis("Vertical");
-        var dir = new Vector2(x, y).normalized;
-        _rigidbody.velocity = dir * _speedCompensation;
+        foreach(var motion in _data.Motions)
+        {
+            transform.position += motion.Play(_data.Speed);
+        }
     }
 
     private void Fire()
