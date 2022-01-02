d414fc1477f30cb8c51d6dadff4b8a134686f9d9
nmskm240 <bokaronogod@gmail.com>
2022-01-02 17:41:36 +0900

    refact ship movement

--- Assets/Project/Scripts/ShipData.cs
+++ Assets/Project/Scripts/ShipData.cs
@@ -1,4 +1,3 @@
-using System.Collections.Generic;
 using UnityEngine;
 
 [CreateAssetMenu(fileName = "ShipData", menuName = "Shooting/ShipData", order = 0)]
@@ -14,13 +13,10 @@ public class ShipData : ScriptableObject
     private float _speed;
     [SerializeField]
     private float _fireRate;
-    [SerializeReference, SubclassSelector]
-    private List<IMotion> _motions;
 
     public string Name { get { return _name; } }
     public string Detail { get { return _detail; } }
     public int HP { get { return _hp; } }
     public float Speed  { get { return _speed; } }
     public float FireRate { get { return _fireRate; } }
-    public IEnumerable<IMotion> Motions { get { return _motions; } }
 }
