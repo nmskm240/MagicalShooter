22c5b9b3702d2c9c969e9a91712d75db80e9a8d7
nmskm240 <bokaronogod@gmail.com>
2022-01-05 13:44:53 +0900

    create dead flow

--- Assets/Project/Scripts/Enemies/EnemyData.cs
+++ Assets/Project/Scripts/Enemies/EnemyData.cs
@@ -21,35 +21,43 @@ namespace Shooting.Enemies
         [SerializeReference, SubclassSelector]
         private List<IMotion> _motions;
         [NonSerialized]
-        private int _currentHP;
+        private int _beforeHP;
+        [NonSerialized]
+        private ReactiveProperty<int> _currentHP = new ReactiveProperty<int>();
         [NonSerialized]
         private ReactiveProperty<IMotion> _currentMotion = new ReactiveProperty<IMotion>();
+        private Subject<int> _onDamaged = new Subject<int>();
         private Subject<Unit> _onDead = new Subject<Unit>();
 
         public string Name { get { return _name; } }
         public string Detail { get { return _detail; } }
         public int MaxHP { get { return _maxHP; } }
-        public int CurrentHP { get { return _currentHP; } }
+        public int CurrentHP { get { return _currentHP.Value; } }
         public float Speed { get { return _speed; } }
         public IEnumerable<IMotion> Motions { get { return _motions; } }
         public IObservable<IMotion> OnMotionChanged { get { return _currentMotion; } }
+        public IObservable<int> OnChangedHP { get { return _currentHP; } }
+        public IObservable<int> OnDamaged { get { return _onDamaged; } }
         public IObservable<Unit> OnDead { get { return _onDead; } }
-        public bool CanHit { get { return 0 < _currentHP; } }
+        public bool CanHit { get { return 0 < CurrentHP; } }
 
         protected virtual void OnEnable()
         {
-            _currentHP = _maxHP;
+            _currentHP.Value = _maxHP;
+            _beforeHP = CurrentHP;
             _currentMotion.Value = _motions.First();
+            OnChangedHP
+                .Select(hp => Math.Abs(hp - _beforeHP))
+                .Where(damage => 0 < damage)
+                .Subscribe(damage => _onDamaged.OnNext(damage));
+            OnDamaged
+                .Where(_ => CurrentHP <= 0)
+                .Subscribe(_ => _onDead.OnNext(Unit.Default));
         }
 
         public virtual void Damage(int power)
         {
-            _currentHP -= power;
-            if (_currentHP <= 0)
-            {
-                _currentHP = 0;
-                _onDead.OnNext(Unit.Default);
-            }
+            _currentHP.Value -= power;
         }
     }
 }
