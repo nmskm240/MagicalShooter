8f1fb1331bb1442f0a2ece4d29c40dc411d04a3c
nmskm240 <bokaronogod@gmail.com>
2022-01-08 13:39:47 +0900

    auto motion

--- Assets/Project/Scripts/Bullets/BulletData.cs
+++ Assets/Project/Scripts/Bullets/BulletData.cs
@@ -9,7 +9,7 @@ using Shooting.Motions;
 namespace Shooting.Bullets
 {
     [CreateAssetMenu(fileName = "BulletData", menuName = "Shooting/BulletData", order = 0)]
-    public class BulletData : ScriptableObject
+    public class BulletData : ScriptableObject, IAutoMotion
     {
         [SerializeField]
         private string _name;
@@ -21,31 +21,11 @@ namespace Shooting.Bullets
         private float _speed;
         [SerializeField]
         private List<MotionData> _motions;
-        [NonSerialized]
-        private ReactiveProperty<MotionData> _currentMotion = new ReactiveProperty<MotionData>();
 
         public string Name { get { return _name; } }
         public string Detail { get { return _detail; } }
         public int Power { get { return _power; } }
         public float Speed { get { return _speed; } }
         public IEnumerable<MotionData> Motions { get { return _motions; } }
-        public IObservable<MotionData> OnMotionChanged { get { return _currentMotion; } }
-
-        private void OnEnable()
-        {
-            _currentMotion.Value = _motions.First();
-        }
-
-        public void DoMove(Transform transform)
-        {
-            var sequence = DOTween.Sequence();
-            foreach (var motion in _motions)
-            {
-                sequence.Append(motion.ToSequence(transform, _speed));
-            }
-            sequence
-                .Play()
-                .OnComplete(() => DoMove(transform));
-        }
     }
 }
