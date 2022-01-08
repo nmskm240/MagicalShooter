53708b067852d1e5f2c375572f97cb98add6ef31
nmskm240 <bokaronogod@gmail.com>
2022-01-08 21:33:34 +0900

    player movement restrictions

--- Assets/Project/Scripts/Characters/Players/Player.cs
+++ Assets/Project/Scripts/Characters/Players/Player.cs
@@ -14,12 +14,22 @@ namespace Shooting.Characters.Players
 
         private void Start()
         {
+            var min = Camera.main.ViewportToWorldPoint(Vector2.zero);
+            var max = Camera.main.ViewportToWorldPoint(Vector2.one);
             var moveInputStream = this.UpdateAsObservable()
                 .Select(_ => new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical")));
             this.FixedUpdateAsObservable()
                 .WithLatestFrom(moveInputStream, (_, input) => input)
                 .Select(input => input.normalized * Time.deltaTime * _model.Speed)
-                .Subscribe(vector => transform.Translate(vector));
+                .Subscribe(vector => 
+                {
+                    var pos = (Vector2)transform.position;
+                    var moved = pos + vector;
+                    var x = Mathf.Clamp(moved.x, min.x, max.x);
+                    var y = Mathf.Clamp(moved.y, min.y, max.y);
+                    var clamped = new Vector2(x - pos.x, y - pos.y);
+                    transform.Translate(clamped);
+                });
 
             this.UpdateAsObservable()
                 .Where(_ => Input.GetKey(KeyCode.Space))
