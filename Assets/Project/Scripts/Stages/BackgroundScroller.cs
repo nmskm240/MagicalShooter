439ccb1248d5a2696663d3bb80917899057f7799
nmskm240 <bokaronogod@gmail.com>
2022-01-11 09:43:24 +0900

    scroll stage background

---
+++ Assets/Project/Scripts/Stages/BackgroundScroller.cs
@@ -0,0 +1,29 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Shooting.Stages
+{
+    public class BackgroundScroller : MonoBehaviour
+    {
+        [SerializeField]
+        private float _speed;
+        [SerializeField]
+        private Vector2 _startPos;
+        [SerializeField]
+        private Vector2 _endPos;
+        [SerializeField]
+        private List<Transform> _transforms;
+
+        private void Update()
+        {
+            foreach (var transform in _transforms)
+            {
+                transform.Translate(-_speed, 0, 0);
+                if (transform.position.x <= _endPos.x)
+                {
+                    transform.position = _startPos;
+                }
+            }
+        }
+    }
+}
