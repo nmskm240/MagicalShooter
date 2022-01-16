7f4f0ff267a4b867d11fd7ae68d1f647d97d477c
nmskm240 <bokaronogod@gmail.com>
2022-01-16 22:17:45 +0900

    fix presenter

--- Assets/Project/Scripts/Presenter.cs
+++ Assets/Project/Scripts/Presenter.cs
@@ -1,15 +1,19 @@
 using UnityEngine;
 
-namespace Shooting.Utils
+namespace Shooting
 {
     public abstract class Presenter<TModel, TView> : MonoBehaviour
+        where TModel : ScriptableObject
         where TView : MonoBehaviour
     {
+        [SerializeField]
+        private TModel _original;
         protected TModel _model { get; set; }
         protected TView _view { get; set; }
 
         protected virtual void Awake()
         {
+            _model = Instantiate<TModel>(_original);
             _view = GetComponent<TView>();
         }
     }
